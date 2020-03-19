855b0e139b71400c174f5ad95287631318d8011b
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-19 14:37:15 -0500

    About page compiles in React on npm start

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/about"]}>
+            <About />
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+          <Route exact path='./portfolio'>
+            <Portfolio />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
