36f6e9d0efd47df32ce38beddbcb88f00909c562
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-22 15:20:35 -0500

    added functionality to map unordered list for technologies used in table on portfolio page

--- client/src/components/unorderedList/index.js
+++ client/src/components/unorderedList/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './style.css';
 
 
-function unorderderList(props) {
+export function UnorderList(props) {
     // console.log(props.key);
     return (
         <ul className={props.bootStrapList ? 'list-group' : ''}>
-            <li>Click on column headers "full name" and "Years With Company" to flip employees from ascending to descending</li>
-            <li>The search bar below and pulldown allow for the employees to be filtered by the selected criteria in dropdown</li>
-            <li>To get all the employees back select the All Employees option in dropdown and type any letter</li>
+            {props.children}
         </ul>
     )
 }
 
-export default unorderderList;
+export function ListItem(props) {
+   return  <li>{props.children}</li>
+}
--- client/src/pages/portfolio.js
+++ client/src/pages/portfolio.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Row } from '../components/Grid';
 import TableRow from '../components/tableRow';
+import { UnorderList, ListItem } from '../components/unorderedList';
 import portfolioObject from '../utils/projects';
 
 
@@ -19,36 +20,45 @@ function Portfolio() {
                         <h3>Portfolio</h3>
                     </div>
                     <div className='table-responsive'>
-                    <table className="table">
-                                <thead className="thead-dark">
-                                    <tr>
-                                        <th scope='col' style={{width: '5%'}}>#</th>
-                                        <th scope='col' style={{width: '10%'}}>Title</th>
-                                        <th scope='col' style={{width: '20%'}}/*onClick={e => this.onSortString(e, 'firstName', this.state.ascKey)}*/>Image</th>                                        
-                                        <th scope='col' style={{width: '20%'}}>Functional Preview</th>
-                                        <th scope='col' style={{width: '15%'}}>Deployed App Link</th>
-                                        <th scope='col' style={{width: '30%'}}/*onClick={e => this.onSortNumber(e, 'yearsWithCompany', this.state.ascKey)}*/>Description</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        portfolioObject.map((value, index) => {
-                                            console.log(value);
-                                            return <TableRow
-                                                scope="row"
-                                                key={index}
-                                                num={index}
-                                                image={value.imgLink}
-                                                appLink={value.appLink}
-                                                title={value.title}
-                                                gifLink={value.functionalPreview}
-                                                desc={value.description}
-                                            />
-                                        })
-                                    }
-                                </tbody>
-                            </table>
-                            </div>
+                        <table className="table">
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th scope='col' style={{ width: '5%' }}>#</th>
+                                    <th scope='col' style={{ width: '5%' }}>Title</th>
+                                    <th scope='col' style={{ width: '25%' }}/*onClick={e => this.onSortString(e, 'firstName', this.state.ascKey)}*/>Image</th>
+                                    <th scope='col' style={{ width: '20%' }}>Functional Preview</th>
+                                    <th scope='col' style={{ width: '10' }}>Deployed App Link</th>
+                                    <th scope='col' style={{ width: '30%' }}/*onClick={e => this.onSortNumber(e, 'yearsWithCompany', this.state.ascKey)}*/>Description</th>
+                                    <th scope='col' style={{ width: '5%' }}>Technologies Used</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    portfolioObject.map((value, index) => {
+                                        console.log(value);
+                                        return <TableRow
+                                            scope="row"
+                                            key={index}
+                                            num={index}
+                                            image={value.imgLink}
+                                            appLink={value.appLink}
+                                            title={value.title}
+                                            gifLink={value.functionalPreview}
+                                            desc={value.description}
+                                        >
+                                            <UnorderList>
+                                                    {
+                                                    value.tech.map((value, index) => {
+                                                        return <ListItem key={index}>{value}</ListItem>
+                                                    })
+                                                    }
+                                            </UnorderList>
+                                        </TableRow>
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 {/* <div className="col-lg-1"></div> */}
             </Row>
