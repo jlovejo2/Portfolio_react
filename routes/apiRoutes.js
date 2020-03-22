74a3894efb76ad158252b29088c73a7109e0f552
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-22 07:42:10 -0500

    finished nodeMailer functionality with react

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const emailSender = require('../nodeMailer');
+const emailSender = require('../controllers/nodeMailer');
 
 router.post('/sendEmail/', function (req, res) {
         
