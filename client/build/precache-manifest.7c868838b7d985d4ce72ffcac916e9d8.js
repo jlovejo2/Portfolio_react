36274ee23e9adc2487749dce40d0effb8b67953c
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-23 18:12:10 -0500

    ran npm run build

---
+++ client/build/precache-manifest.7c868838b7d985d4ce72ffcac916e9d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cf74624e4e108a615ff1bbf36e29cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d20aa6fea4bc39ca21",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1b9b0c6218d1fa222e3d",
+    "url": "/static/css/main.0a4541a3.chunk.css"
+  },
+  {
+    "revision": "f0d20aa6fea4bc39ca21",
+    "url": "/static/js/2.b79abf2f.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.b79abf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9b0c6218d1fa222e3d",
+    "url": "/static/js/main.74e1152f.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "6680427f577dc64cc0355a957a7932ae",
+    "url": "/static/media/IMG_9209.6680427f.JPG"
+  }
+]);
