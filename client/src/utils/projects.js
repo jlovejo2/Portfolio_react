71d4b6956b52d6bbc45d9d1e9be5bc359bf01e08
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-23 17:12:26 -0500

    updated technologies used for projects on portfolio

--- client/src/utils/projects.js
+++ client/src/utils/projects.js
@@ -5,7 +5,7 @@ const portfolioObject = [
         title: "Nutridish",
         functionalPreview: 'assets/gifs/nutridish.gif',
         description: 'The purpose of this app is for the user to either login or create an account.  Once that has been done they can search for recipes based on nutrient and diet selection.  These recipes can be saved in their account and referenced for later use.',
-        tech: ['react', 'javascript', 'SQL', 'sequelize']
+        tech: ['react', 'JavaScript', 'SQL', 'sequelize', 'Node.js', 'Handlebars', 'user-authentication', 'Bootstrap', 'Edamam API']
     },
     {
         imgLink: "assets/images/TeamHtml.png",
@@ -13,7 +13,7 @@ const portfolioObject = [
         title: "Team Generator",
         functionalPreview: 'assets/gifs/team-generator.gif',
         description: 'The purpose of this app is for the user to create a quick an easy html to easiliy reference the team members contact info.',
-        tech: []
+        tech: ['JavaScript', 'Node.js', 'Inquirer', 'Jest', 'Bootstrap']
     },
     {
         imgLink: "assets/images/searchApp.jpeg",
@@ -21,7 +21,7 @@ const portfolioObject = [
         title: "Multi Media Search App",
         functionalPreview: 'assets/gifs/multi-media-search.gif',
         description: 'The purpose of this app is to search for books, games, and movies based on the same keyword or title.',
-        tech: []
+        tech: ['JavaScript, jQuery, Materialize, HTML, OMDB API, Google Books API, RAWG API']
     },
     {
         imgLink: "assets/images/trivia_quiz.jpg",
@@ -29,7 +29,7 @@ const portfolioObject = [
         title: "Trivia Quiz",
         functionalPreview: 'assets/gifs/trivia-quiz.gif',
         description: 'This app is a coding trivia quiz.  It generates the questions one at a time with a score being based on a timer that begins to run as soon as the quiz is started. Seconds are added for a correct answer and subtracted for an incorrect answer.',
-        tech: []
+        tech: ['JavaScript', 'jQuery', 'Bootstrap']
     },
     {
         imgLink: "assets/images/weather_image.png",
@@ -37,7 +37,7 @@ const portfolioObject = [
         title: "Weather Dashboard",
         functionalPreview: '',
         description: 'This is a weather dashboard app that will display the current weather as well as a five day forecast based on a location search.  Past searches are stored in the browser for ease of reference.',
-        tech: []
+        tech: ['Javascript', 'jQuery', 'Bootstrap']
     },
     {
         imgLink: "assets/images/calendar.jpg",
@@ -45,7 +45,7 @@ const portfolioObject = [
         title: "Work Day Calendar",
         functionalPreview: 'assets/gifs/work-day-calendar.gif',
         description: 'This is a calendar to keep track of events and tasks during the work day.  The calendar is dynamic and shows the current hour block in green, with past hour blocks in grey.',
-        tech: []
+        tech: ['JavaScript', 'jQuery', 'momnet.js', 'Bootstrap',]
     },  
     {
         imgLink: "assets/images/password.jpg",
@@ -53,7 +53,7 @@ const portfolioObject = [
         title: "Password Generator",
         functionalPreview: '',
         description: 'This is website that will generate a password based on the desired criteria by user.  User specifies the length and whether they want numbers, uppercase and lowercase letters, and special characters in their password.',
-        tech: []
+        tech: ['Javascript', 'jQuery', 'Bootstrap']
     }
 
 ];
