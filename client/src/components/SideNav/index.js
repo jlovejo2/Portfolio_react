0905a0ac503452b7f17e654c989467cebd3a976d
James Lovejoy <james.lovejoy2@gmail.com>
2020-05-12 21:02:54 -0500

    added contact me button with webkit animation

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./style.css";
 
 function SideNav(props) {
   return (
-    <div id="mySidenav" /*class="vertText"*/>
-      <button onClick={props.activeContact} className="btn btn-link">
-        Contact Me
+    <div className="sideNavDiv" /*class="vertText"*/>
+      <button
+        id="mySideNav"
+        onClick={props.activeContact}
+        className="btn btn-link"
+      >
+        {/* <FontAwesomeIcon icon={faPaperPlane} style={{ color: "white" }} /> */}
+        Click to Contact Me
       </button>
     </div>
   );
