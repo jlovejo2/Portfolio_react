d5944954a9cfdf97b120c5a73a5b36947b7cfe83
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-13 16:12:15 -0500

    fixed formatting in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-
-    //Email me with contact form information calls apiroutes.js in routes folder
-    nodeMailerSendInfo: function(emailContent) {
-        return axios.post('/sendEmail/', emailContent);
-    }
-
-}
+  //Email me with contact form information calls apiroutes.js in routes folder
+  nodeMailerSendInfo: function (emailContent) {
+    return axios.post("/sendEmail/", emailContent);
+  },
+};
