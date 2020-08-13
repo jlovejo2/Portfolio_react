58830157e2311518296b1437570cb03a8db2d256
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-13 16:12:04 -0500

    fixed formatting in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,22 @@
 //This variable is calling the path package so that we are able to cal our html files
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const emailSender = require('../controllers/nodeMailer');
+const emailSender = require("../controllers/nodeMailer");
 
-router.post('/sendEmail/', function (req, res) {
-        
-    console.log(req.body);
-    //Send the
-    const sender = req.body.email;
-    const emailSubject = req.body.subject;
-    const emailText = `My name is: ${req.body.name}
+router.post("/sendEmail/", function (req, res) {
+  console.log(req.body);
+  //Send the
+  const sender = req.body.email;
+  const emailSubject = req.body.subject;
+  const emailText = `My name is: ${req.body.name}
     Here is my message: ${req.body.body}`;
 
-   emailSender('james.lovejoy2@gmail.com', sender, emailSubject, emailText )
-    .then( responseObj => res.send(true))
-    .catch(console.error)
-    });
+  emailSender("james.lovejoy2@gmail.com", sender, emailSubject, emailText)
+    .then((responseObj) => res.send(true))
+    .catch(console.error);
+});
+
+router.get("/api/getImages", function (req, res) {});
 
 module.exports = router;
